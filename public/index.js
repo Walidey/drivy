f2f8702b9348200ad457aed1f0713eeb606c2019
Wahhlid <err.walid@gmail.com>
2017-01-17 22:36:45 +0100

    J'ai eu un probleme pour push touts les exercices 1 par 1

--- public/index.js
+++ public/index.js
@@ -22,6 +22,7 @@ var cars = [{
 //The `price` is updated from exercice 1
 //The `commission` is updated from exercice 3
 //The `options` is useful from exercice 4
+
 var rentals = [{
   'id': '1-pb-92',
   'driver': {
@@ -154,6 +155,7 @@ var actors = [{
   }]
 }];
 
+
 //list of rental modifcation
 //useful for exercise 6
 var rentalModifications = [{
@@ -165,6 +167,97 @@ var rentalModifications = [{
   'pickupDate': '2015-12-05'
 }];
 
+function RecuperationPrice (rental){
+	
+	var carId = rental.carId;
+	for (var i = 0 ; i< cars.length; i++)
+	{
+		if (carId == cars[i].id)
+		{
+			var result = [cars[i].pricePerKm, cars[i].pricePerDay] ;
+		}
+	}
+
+	return result;
+}
+function dateDiff(date1, date2){
+    var diff = {}                           // Initialisation du retour
+    var tmp = date2 - date1;
+ 
+    tmp = Math.floor(tmp/1000);             // Nombre de secondes entre les 2 dates
+    diff.sec = tmp % 60;                    // Extraction du nombre de secondes
+ 
+    tmp = Math.floor((tmp-diff.sec)/60);    // Nombre de minutes (partie entière)
+    diff.min = tmp % 60;                    // Extraction du nombre de minutes
+ 
+    tmp = Math.floor((tmp-diff.min)/60);    // Nombre d'heures (entières)
+    diff.hour = tmp % 24;                   // Extraction du nombre d'heures
+     
+    tmp = Math.floor((tmp-diff.hour)/24);   // Nombre de jours restants
+    diff.day = tmp;
+     
+    return diff.day+1;
+}
+
+function DiffRent(rental){
+	var date1 = new Date(rental.returnDate);
+	var date2 = new Date(rental.pickupDate);
+	return dateDiff(date2,date1);
+}
+
+function price (rental){
+
+	rental.price = rental.distance * RecuperationPrice(rental)[0] + DiffRent(rental)* RecuperationPrice(rental)[1];
+	if (DiffRent(rental) >=1 && DiffRent(rental) < 4 )
+	{
+		rental.price = rental.price - (rental.price*10)/100;
+	}
+
+	if (DiffRent(rental) >=4 && DiffRent(rental) < 10 )
+	{
+		rental.price = rental.price - (rental.price*30)/100;
+	}
+	if (DiffRent(rental) >=10)
+	{
+		rental.price = rental.price - (rental.price*50)/100;
+	}
+	rental.commission.assistance = DiffRent(rental);
+	rental.commission.insurance = ((rental.price*30)/100)/2;
+	rental.commission.drivy = ((rental.price*30)/100) - (rental.commission.insurance + rental.commission.assistance);
+	if (rental.options.deductibleReduction == true)
+	{
+		rental.price+= 4* DiffRent(rental);//EXERCICE 4
+	}
+}
+
+function RecupRent(actor)
+{
+	var rentalId = actors.rentalId;
+	for (var i =0; i< rentals.length;i++)
+	{
+		if (rentalId == rentals[i].id)
+		{
+			var result = rentals[i];
+		}
+	}
+
+	return result;
+
+}
+
+function Paye(actor)
+{
+	var rental = RecupRent(actor);
+	actor.payment[0].amount = rental.price; //driver
+	actor.payment[1].amount = (rental.price*70)/100; //Owner
+	actor.payment[2].amount= rental.commission.insurance ; //insurance
+	actor.payment[3].amount= rental.commission.assistance;
+	actor.payment[4].amount = rental.commission.drivy;
+		if (rental.options.deductibleReduction == true)
+	{
+		actor.payment[4].amount+= 4* DiffRent(rental);
+	}
+}
 console.log(cars);
 console.log(rentals);
 console.log(actors);
